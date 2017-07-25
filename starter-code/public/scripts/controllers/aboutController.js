9bf832615136e3e8e6f00e2cce934d0e0ecc71e6
Madeline Stevens <stevensm1987@gmail.com>
2017-07-24 17:26:59 -0700

    completed all TODOs but we can't just type in localhost:3000/about, we get a 404 status error

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,7 +4,7 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // TODO: Define a function that hides all main section elements, and then reveals just the #about section:
+  // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.initAboutController = function() {
     $('.tab-content').hide();
     $('#about').show();
